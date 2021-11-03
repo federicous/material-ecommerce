4986f7f536902ddc7d8d4df18faf38e95f722455
Fede <federicous@gmail.com>
2021-11-02 23:24:16 -0300

    menu fix

--- src/components/HomePage2/HomePage2.jsx
+++ src/components/HomePage2/HomePage2.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 
 
 const navList =[
-	{id:"1", name:"Laptops", value:"abcat0502000", image:"https://assets.cybermonday.com.ar/uploads/tips/97/orig-1603762372LENOVO.png"},
-	{id:"2", name:"Phones", value:"pcmcat209400050001", image:"https://jobcompass.net/wp-content/uploads/2020/03/Sell-Your-Old-Cell-Phone-To-Both-Online-and-Offline.jpg"},
-	{id:"3", name:"TVs", value:"abcat0101000", image:"https://mastertechhome.com/wp-content/uploads/2020/02/smart-tvs.jpg"},
+	{id:"1", name:"Laptops", value:"laptops", image:"https://assets.cybermonday.com.ar/uploads/tips/97/orig-1603762372LENOVO.png"},
+	{id:"2", name:"Phones", value:"phones", image:"https://jobcompass.net/wp-content/uploads/2020/03/Sell-Your-Old-Cell-Phone-To-Both-Online-and-Offline.jpg"},
+	{id:"3", name:"TVs", value:"tvs", image:"https://mastertechhome.com/wp-content/uploads/2020/02/smart-tvs.jpg"},
       ]
 
 const ImageButton = styled(ButtonBase)(({ theme }) => ({
