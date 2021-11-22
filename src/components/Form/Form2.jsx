cc3e403b143f81ca07cf7cfc3aca093650bb8538
Fede <federicous@gmail.com>
2021-11-22 17:25:54 -0300

    warning email

--- src/components/Form/Form2.jsx
+++ src/components/Form/Form2.jsx
@@ -1,13 +1,17 @@
-import { Button, TextField } from "@material-ui/core";
+import { Alert, Button, TextField } from "@material-ui/core";
 import { Box } from "@material-ui/system";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export default function Form2({orderGenerate}) {
   const { register, handleSubmit } = useForm();
+  const [emailError, setEmailError] = useState(false)
   const onSubmit = data => {
 	  if (data.email === data.email2){
 		  orderGenerate(data)
+		  setEmailError(false)
+	  } else{
+		  setEmailError(true)
 	  }
 	};
    
@@ -26,11 +30,15 @@ export default function Form2({orderGenerate}) {
 		>
 			<TextField  id="outlined-name" label="Name" {...register("firstName", { required: true, pattern: /^[A-Za-z]+$/i })} />
 			<TextField  id="outlined-name" label="LastName" {...register("lastName", {  required: true, pattern: /^[A-Za-z]+$/i })} />
-			<TextField  id="outlined-name" label="Phone" type="phone" {...register("phone", { required: true,  pattern: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g })} />
+			<TextField  id="outlined-name" label="Phone" type="phone" {...register("phone", { required: true,  pattern: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/g })} />
 			<TextField  id="outlined-name" label="Email" type="email" {...register("email", { required: true })} />
 			<TextField  id="outlined-name" label="Email2" type="email" {...register("email2", { required: true})} />
 			<Button type="submit" color="primary" size="small" variant="contained">Purchase</Button>
-			
+			{emailError? (
+				<Alert severity="warning">The email addresses are different</Alert>
+			):(
+			<></>	
+			)}
 		</Box>
     	</form>
   );
